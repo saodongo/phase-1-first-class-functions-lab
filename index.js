5f1e4f2f510947e55058bdcbdd3db028133ee55c
sharonodongo <sharon94odongo@gmail.com>
2024-09-25 22:33:49 +0300

    submitting assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); // ['Antonia', 'Nuru']
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); // ['Amari', 'Mo']
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);  // Declared fareDoubler here
+console.log(fareDoubler(10)); // 20
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(10)); // 30
+
+const fareQuadrupler = createFareMultiplier(4);
+console.log(fareQuadrupler(5)); // 20
+
+const selectDifferentDrivers = function(drivers, whichDrivers) {
+    return whichDrivers(drivers);
+};
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers)); // ['Antonia', 'Nuru']
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers)); // ['Amari', 'Mo']
+
